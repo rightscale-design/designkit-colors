e43895f16f45f2fbae14898ddcc4a90230a71f13
jasonmelgoza <jason@rightscale.com>
2018-02-13 09:27:09 -0800

    Updated build setup

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,11 +1,11 @@
 // build.js
 var _ = require('lodash')
 var fs = require('fs')
-var parseColors = require('./lib/parse-colors')
+var parseColors = require('../lib/parse-colors')
 
-var css = fs.readFileSync('./css/designkit-colors.css', 'utf8')
+var css = fs.readFileSync('./dist/designkit-colors.css', 'utf8')
 // Read the template string
-var template = fs.readFileSync('template.html', 'utf8')
+var template = fs.readFileSync('./templates/colors.html', 'utf8')
 // Create a lodash template function
 var tpl = _.template(template)
 var colors = parseColors(css)
@@ -14,4 +14,4 @@ var colors = parseColors(css)
 var html = tpl({ colors: colors })
 
 // Write an HTML file to disk
-fs.writeFileSync('index.html', html)
+fs.writeFileSync('./docs/colors.html', html)
